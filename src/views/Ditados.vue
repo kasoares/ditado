4197a09e8d26dec5b835c06d6844ee15d7943abd
joaolucasmatera <joaolucas.matera@gmail.com>
2025-12-16 23:23:25 -0300

    UI: remover botão Visualizar das ações (manter descrição e demais botões)

--- src/views/Ditados.vue
+++ src/views/Ditados.vue
@@ -170,9 +170,11 @@
         Biblioteca de Ditados Cadastrados
       </v-card-title>
 
-      <div class="pa-4 pt-2 pb-0">
-        <div class="text-caption text-grey-darken-1">Clique em um ditado para visualizar os detalhes. Ou use o botão "Visualizar" em Ações.</div>
-      </div>
+      <v-card-text class="pa-2 pt-0">
+        <div class="text-caption text-grey-darken-1">
+          Clique em um ditado para ver os detalhes. Também é possível usar o botão "Visualizar" em Ações.
+        </div>
+      </v-card-text>
 
       <v-data-table
         v-if="ditadosFiltrados.length > 0"
@@ -221,14 +223,6 @@
 
         <template v-slot:item.acoes="{ item }">
           <div class="d-flex gap-2 justify-end" @click.stop>
-            <v-btn
-              icon="mdi-eye"
-              size="small"
-              variant="text"
-              color="primary"
-              title="Visualizar detalhes"
-              @click="abrirDetalhesSimples(item)"
-            />
             <v-btn
               icon="mdi-school"
               size="small"
@@ -600,12 +594,6 @@ async function abrirDetalhesDitado(event, { item }) {
     console.error('Erro ao carregar detalhes do ditado:', erro);
     mostrarSnackbar('Não foi possível carregar os detalhes do ditado.', 'warning');
   }
-}
-
-// Helper para abrir detalhes a partir de ações (botão)
-function abrirDetalhesSimples(item) {
-  // Reaproveita a função existente que espera (event, { item })
-  abrirDetalhesDitado(null, { item })
 }
 
 function getAudioSrc(base64) {
